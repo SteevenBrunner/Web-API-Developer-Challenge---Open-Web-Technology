753b0edbfbb8b79d54a3a1ef76ab6d1c636ba9df
Steeven Brunner <steevenbrunner@gmail.com>
2020-08-10 03:28:01 +0200

    Adding models + small properties changes

---
+++ Models/Skills.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web_API_Developer_Challenge___Open_Web_Technology.Models
+{
+    public class Skills
+    {
+        public enum Levels
+        {
+            Beginner,
+            Intermediate,
+            Advanced,
+            Expert
+        }
+
+        public String Name { get; set; }
+
+        public Levels Level { get; set; }
+
+    }
+}
+s
